84fdad904a5adcd86971ac917b843477a0ca6ad9
Emily Wang <ezwang@berkeley.edu>
2019-12-03 21:57:38 -0800

    Updates

---
+++ precache-manifest.4b33e42208dd78e78844fe54b5651eb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "378956d37179d3e86c21e3e3ebc9f16b",
+    "url": "/StockAssistant/index.html"
+  },
+  {
+    "revision": "be285ae8420b77030b9f",
+    "url": "/StockAssistant/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "7a6116ff5907f6a4cb4c",
+    "url": "/StockAssistant/static/js/2.df8e1dcb.chunk.js"
+  },
+  {
+    "revision": "be285ae8420b77030b9f",
+    "url": "/StockAssistant/static/js/main.2899f3e4.chunk.js"
+  },
+  {
+    "revision": "d0bdf532891ba9288c27",
+    "url": "/StockAssistant/static/js/runtime-main.b5027f70.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/StockAssistant/static/media/logo.25bf045c.svg"
+  }
+]);
